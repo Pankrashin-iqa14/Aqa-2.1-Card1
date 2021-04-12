16ca562713548fc210f2450f7af0c6a0d1ae0122
Aleksandr <pankrashin.al@yandex.ru>
2021-04-12 20:58:20 +0400

    mod test

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -13,7 +13,7 @@ public class CardOrderTest {
         $("[data-test-id=name] input").setValue("Михаил");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
-        $("button").click();
+        $("[type-button]").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
