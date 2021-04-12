668a95de9d81074ecfe5187ba15425148c7d0138
Aleksandr <pankrashin.al@yandex.ru>
2021-04-12 21:59:46 +0400

    mod test

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -9,11 +9,11 @@ import static com.codeborne.selenide.Selenide.open;
 public class CardOrderTest {
     @Test
     void shouldMakeRequest() {
-        open("http://localhost:7777");
+        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Михаил");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
-        $("[type-button]").click();
+        $("[type=button]").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
